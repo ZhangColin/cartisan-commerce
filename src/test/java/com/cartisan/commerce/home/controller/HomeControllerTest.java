11d7e1680613433bf4ebf5b6118252e0585533a4
张锦华 <zhang_colin@163.com>
2017-02-27 11:14:03 +0800

    home controller unit test and integration test

---
+++ src/test/java/com/cartisan/commerce/home/controller/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.cartisan.commerce.home.controller;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class HomeControllerTest {
+    @Test
+    public void indexIntegrationTest() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/")).andExpect(view().name("/home"));
+    }
+
+    @Test
+    public void indexUnitTest() {
+        // Arrange
+        HomeController controller = new HomeController();
+
+        // Act
+        String viewName = controller.index();
+
+        // Assert
+        assertThat(viewName).isEqualTo("/home");
+    }
+
+}
