5ab786353c37146d40f1debee60cbaaef8077a08
张锦华 <zhang_colin@163.com>
2017-02-27 14:03:35 +0800

    add signin page apply emptyLayout for mustache template

---
+++ src/main/java/com/cartisan/commerce/home/controller/SignController.java
@@ -0,0 +1,12 @@
+package com.cartisan.commerce.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignController {
+    @GetMapping("/signin")
+    public String Signin() {
+        return "/signin";
+    }
+}
