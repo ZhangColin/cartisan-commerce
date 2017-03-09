a9479fc9dc4d7a0375d9067a2071284c7b2d81fa
张锦华 <zhang_colin@163.com>
2017-03-09 11:44:02 +0800

    apply rest controller and concurrentMap cache

--- src/main/java/com/cartisan/commerce/Application.java
+++ src/main/java/com/cartisan/commerce/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mustache.MustacheAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = MustacheAutoConfiguration.class)
+@EnableCaching
 public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/cartisan/commerce/home/api/CacheDemoController.java
@@ -0,0 +1,29 @@
+package com.cartisan.commerce.home.api;
+
+import com.cartisan.commerce.home.service.CacheDemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cache_demo")
+public class CacheDemoController {
+    @Autowired
+    private CacheDemoService demoService;
+
+    @GetMapping("create_date")
+    public String create(){
+        return demoService.createDate();
+    }
+
+    @GetMapping("delete_date")
+    public void del(){
+        demoService.deleteDate();
+    }
+
+    @GetMapping("get_date")
+    public String get(){
+        return demoService.getDate();
+    }
+}
